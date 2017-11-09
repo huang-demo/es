08087717ca3a1c739eeffa1329eb1ed739a8bade
dem <dem@gmail.com>
2017-11-09 23:16:54 +0800

    Initial commit

---
+++ src/main/java/com/dem/es/controller/TestController.java
@@ -0,0 +1,16 @@
+package com.dem.es.controller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
+public class TestController {
+
+    @RequestMapping("/sayHello")
+    public String sayHello(){
+        return "hello";
+    }
+}
