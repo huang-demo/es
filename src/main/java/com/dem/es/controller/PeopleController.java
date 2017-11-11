b2fc412c31e719473eaace8ad960ab3cfc7e76a7
dem <dem@gmail.com>
2017-11-11 15:35:34 +0800

    新增删 更新操作

--- src/main/java/com/dem/es/controller/PeopleController.java
+++ src/main/java/com/dem/es/controller/PeopleController.java
@@ -1,9 +1,14 @@
 package com.dem.es.controller;
 
-import org.elasticsearch.action.get.GetRequestBuilder;
+
+import com.dem.es.util.Constant;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.elasticsearch.action.delete.DeleteResponse;
 import org.elasticsearch.action.get.GetResponse;
-import org.elasticsearch.action.index.IndexRequest;
 import org.elasticsearch.action.index.IndexResponse;
+import org.elasticsearch.action.update.UpdateRequest;
+import org.elasticsearch.action.update.UpdateResponse;
 import org.elasticsearch.client.transport.TransportClient;
 import org.elasticsearch.common.xcontent.XContentBuilder;
 import org.elasticsearch.common.xcontent.XContentFactory;
@@ -16,11 +21,14 @@ import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
 import java.util.Date;
+import java.util.concurrent.ExecutionException;
+import java.util.logging.Logger;
 
 @RestController
 @RequestMapping("/people")
 public class PeopleController {
 
+    private static Log log = LogFactory.getLog(PeopleController.class);
     @Autowired
     private TransportClient client;
 
@@ -33,7 +41,7 @@ public class PeopleController {
     @GetMapping("/man/{id}")
     public ResponseEntity getById(@PathVariable String id) {
         if (StringUtils.hasLength(id)) {
-            GetResponse response = client.prepareGet("people", "man", id).get();
+            GetResponse response = client.prepareGet(Constant.INDEX_PEOPLE, Constant.TYPE_MAN, id).get();
             if (response.isExists()) {
                 return new ResponseEntity(response.getSource(), HttpStatus.OK);
             }
@@ -60,7 +68,7 @@ public class PeopleController {
                     .field("country", country)
                     .field("date", date.getTime())
                     .endObject();
-            IndexResponse response = client.prepareIndex("people", "man").setSource(contentBuilder).get();
+            IndexResponse response = client.prepareIndex(Constant.INDEX_PEOPLE, Constant.TYPE_MAN).setSource(contentBuilder).get();
             return new ResponseEntity(response.getId(), HttpStatus.OK);
         } catch (IOException e) {
             e.printStackTrace();
@@ -69,7 +77,37 @@ public class PeopleController {
     }
 
     @DeleteMapping("/man/delete/{id}")
-    public ResponseEntity deleteById(@PathVariable String id){
-        return null;
+    public ResponseEntity deleteById(@PathVariable String id) {
+        try {
+            DeleteResponse res = client.prepareDelete(Constant.INDEX_PEOPLE, Constant.TYPE_MAN, id).get();
+            return new ResponseEntity(res.getResult().toString(), HttpStatus.OK);
+        } catch (Exception e) {
+            log.error(e.getMessage());
+        }
+        return new ResponseEntity(null, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+    @PutMapping("/man/update/{id}")
+    public ResponseEntity updateById(@PathVariable String id,String name,String country,@DateTimeFormat(pattern = "yyyy-MM-dd") Date date){
+//        UpdateRequest req = new UpdateRequest(Constant.INDEX_PEOPLE,Constant.TYPE_MAN,id);
+        try {
+            XContentBuilder builder = XContentFactory.jsonBuilder().startObject();
+            if(StringUtils.hasLength(name)){
+                builder.field("name",name);
+            }
+            if(StringUtils.hasLength(country)){
+                builder.field("country",country);
+            }
+            if(null!=date){
+                builder.field("date",date.getTime());
+            }
+            builder.endObject();
+//            req.doc(builder);
+            UpdateResponse response = client.prepareUpdate(Constant.INDEX_PEOPLE, Constant.TYPE_MAN, id).setDoc(builder).get();
+            return new ResponseEntity(response.getResult().toString(),HttpStatus.OK);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return new ResponseEntity(null, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
---
+++ src/main/java/com/dem/es/util/Constant.java
@@ -0,0 +1,7 @@
+package com.dem.es.util;
+
+public class Constant {
+
+    public static String INDEX_PEOPLE="people";
+    public static String TYPE_MAN="man";
+}
