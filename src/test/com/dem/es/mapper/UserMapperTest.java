f108a5ba896cfadcdeeb3a291edb2e4d052bf271
demo <354079281@qq.com>
2017-11-20 18:55:04 +0800

    修改结构

--- src/test/com/dem/es/mapper/UserMapperTest.java
+++ src/test/com/dem/es/mapper/UserMapperTest.java
@@ -1,16 +1,12 @@
 package com.dem.es.mapper;
 
-import com.dem.es.web.Application;
 import com.dem.es.domain.User;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.transaction.annotation.Transactional;
 
 
 @RunWith(SpringJUnit4ClassRunner.class)
