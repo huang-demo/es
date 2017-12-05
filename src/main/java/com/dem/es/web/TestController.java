fe6f1ba0433d3f4ebf0370793fdcc72b4c46f63e
dem <dem@gmail.com>
2017-12-05 19:31:36 +0800

    update

--- src/main/java/com/dem/es/web/TestController.java
+++ src/main/java/com/dem/es/web/TestController.java
@@ -1,15 +1,14 @@
 package com.dem.es.web;
 
-import com.dem.es.service.JedisClient;
 import com.dem.es.exception.MyException;
 import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiImplicitParam;
-import io.swagger.annotations.ApiImplicitParams;
 import io.swagger.annotations.ApiOperation;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -30,7 +29,7 @@ public class TestController {
     @GetMapping("/sayHello")
     @ResponseBody
     public String sayHello() {
-        return "hello3211";
+        return "hello121";
     }
 
 
--- src/test/com/dem/es/mapper/UserMapperTest.java
+++ src/test/com/dem/es/mapper/UserMapperTest.java
@@ -1,27 +1,13 @@
 package com.dem.es.mapper;
 
-import com.dem.es.domain.User;
-import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @Configuration()
 public class UserMapperTest {
-    @Autowired
-    private UserMapper userMapper;
-
-    @Test
-    @Rollback
-    public void findById() throws Exception {
-        User user = userMapper.findById(25L);
-        System.out.println(user.getId());
-
-    }
 
 
 }
